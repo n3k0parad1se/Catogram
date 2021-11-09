38b7f1e1fa95008121239c2fa21fbb9f1ae205ff
xaxtix <xardas3200@gmail.com>
2021-11-09 04:49:31 +0300

    sdf

--- TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/BuildVars.java
@@ -15,9 +15,10 @@ import android.os.Build;
 public class BuildVars {
 
     public static boolean DEBUG_VERSION = false;
-    public static boolean LOGS_ENABLED = false;
     public static boolean DEBUG_PRIVATE_VERSION = false;
+    public static boolean LOGS_ENABLED = false;
     public static boolean USE_CLOUD_STRINGS = true;
+<<<<<<< HEAD
     public static boolean CHECK_UPDATES = false;
     public static int APP_ID = BuildConfig.APP_ID;
     public static String APP_HASH = BuildConfig.API_HASH;
@@ -27,6 +28,17 @@ public class BuildVars {
     //
     public static String SMS_HASH = "w0lkcmTZkKh";
     public static String PLAYSTORE_APP_URL = "https://t.me/catogram";
+=======
+    public static boolean CHECK_UPDATES = true;
+    public static boolean NO_SCOPED_STORAGE = Build.VERSION.SDK_INT <= 29;
+    public static int BUILD_VERSION = 2466;
+    public static String BUILD_VERSION_STRING = "8.2.3";
+    public static int APP_ID = 4;
+    public static String APP_HASH = "014b35b6184100b085b0d0572f9b5103";
+
+    public static String SMS_HASH = isStandaloneApp() ? "w0lkcmTZkKh" : (DEBUG_VERSION ? "O2P2z+/jBpJ" : "oLeq9AcOZkT");
+    public static String PLAYSTORE_APP_URL = "https://play.google.com/store/apps/details?id=org.telegram.messenger";
+>>>>>>> 44be21dd0 (Update to 8.2.3)
 
     static {
         if (ApplicationLoader.applicationContext != null) {
--- TMessagesProj/src/main/java/org/telegram/messenger/MessageObject.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/MessageObject.java
@@ -5940,6 +5940,7 @@ public class MessageObject {
                 attachPathExists = f.exists();
             }
             if (!attachPathExists) {
+
                 File file = FileLoader.getPathToMessage(messageOwner);
                 if (type == 3 && needDrawBluredPreview()) {
                     mediaExists = new File(file.getAbsolutePath() + ".enc").exists();
